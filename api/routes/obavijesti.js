4b934cb463db40191424046acea03dcfd646d1a2
zeljko94 <zeljko-10000@hotmail.com>
2019-06-12 19:20:33 +0200

    obavijesti

--- api/routes/obavijesti.js
+++ api/routes/obavijesti.js
@@ -17,5 +17,23 @@ router.get('/:id', (req, res, next) => {
         });
 });
 
+router.patch('/open/:id', (req, res, next) => {
+    const id = req.params.id;
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Obavijest.update({ _id: id }, { $set:  updateOps })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "Message set as read!"
+            });
+        })
+        .catch(err => {
+            res.status(500).json({error: err});
+        });
+});
+
 
 module.exports = router;
